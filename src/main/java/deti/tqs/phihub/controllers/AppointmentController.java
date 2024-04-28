77ec88f2d7fd1e10a3ecfff1a55dfa8652644748
FiNeX96 <rodrigoaguiar96@ua.pt>
2024-04-28 12:48:09 +0100

    PHIH-24 appointments list and get by id

--- src/main/java/deti/tqs/phihub/controllers/AppointmentController.java
+++ src/main/java/deti/tqs/phihub/controllers/AppointmentController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 import deti.tqs.phihub.models.Appointment;
 
@@ -16,7 +18,6 @@ import deti.tqs.phihub.services.AppointmentService;
 import deti.tqs.phihub.services.SpecialityService;
 import deti.tqs.phihub.services.UserService;
 import deti.tqs.phihub.DTOs.AppointmentSchema;
-
 
 @RestController
 @RequestMapping("/appointments")
@@ -57,48 +58,21 @@ public class AppointmentController {
         return ResponseEntity.ok(appointment);
     }
 
-    /*
-     * @GetMapping
-     * public ResponseEntity<List<Appointment>> getAppointments() {
-     * var user = userService.getUserFromContext();
-     * var appointments = appointmentService.getAppointmentsByPatient(user);
-     * return ResponseEntity.ok(appointments);
-     * }
-     * 
-     * 
-     * @GetMapping("/{id}")
-     * public ResponseEntity<Appointment> getAppointment(@PathVariable Long id) {
-     * var user = userService.getUserFromContext();
-     * var appointment = appointmentService.getAppointmentById(id);
-     * if (appointment.getPatient().getId() != user.getId()) {
-     * return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-     * }
-     * return ResponseEntity.ok(appointment);
-     * }
-     * 
-     * @DeleteMapping("/{id}")
-     * public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
-     * var user = userService.getUserFromContext();
-     * var appointment = appointmentService.getAppointmentById(id);
-     * if (appointment.getPatient().getId() != user.getId()) {
-     * return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-     * }
-     * appointmentService.deleteAppointment(appointment);
-     * return ResponseEntity.ok().build();
-     * }
-     */
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAppointments() {
+        var user = userService.getUserFromContext();
+        List<Appointment> appointments = appointmentService.getAppointmentsByPatient(user);
+        return ResponseEntity.ok(appointments);
+    }
 
-    /*
-     * @PostMapping("/appointments/{id}/pay")
-     * public ResponseEntity<Bill> payAppointment(@PathVariable Long id) {
-     * var user = userService.getUserFromContext();
-     * var appointment = appointmentService.getAppointmentById(id);
-     * if (appointment.getPatient().getId() != user.getId()) {
-     * return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-     * }
-     * var bill = billService.createBill(appointment);
-     * // ... rest of the logic for paying the appointment
-     * }
-     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointment(@PathVariable Long id) {
+        var user = userService.getUserFromContext();
+        Appointment appointment = appointmentService.getAppointmentById(id);
+        if (appointment.getPatient().getId() != user.getId()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return ResponseEntity.ok(appointment);
+    }
 
 }
