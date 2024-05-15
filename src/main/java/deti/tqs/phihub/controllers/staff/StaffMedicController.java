a6fb3be3bbfae30913493b2ae690e98402a1ff03
FiNeX96 <rodrigoaguiar96@ua.pt>
2024-05-15 20:52:40 +0100

    PHIH-50 fixed medics add endpoint

--- src/main/java/deti/tqs/phihub/controllers/staff/StaffMedicController.java
+++ src/main/java/deti/tqs/phihub/controllers/staff/StaffMedicController.java
@@ -2,14 +2,13 @@ package deti.tqs.phihub.controllers.staff;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
-import deti.tqs.phihub.dtos.MedicSchema;
 import deti.tqs.phihub.models.Medic;
 import deti.tqs.phihub.models.Speciality;
 import deti.tqs.phihub.services.MedicService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import deti.tqs.phihub.dtos.MedicSchema;
 
 
 import org.springframework.http.ResponseEntity;
@@ -30,6 +29,11 @@ public class StaffMedicController {
 
         Medic medic = new Medic();
         medic.setName(medicSchema.name());
+        medic.setAge(medicSchema.age());
+        medic.setEmail(medicSchema.email());
+        medic.setPhone(medicSchema.phone());
+        medic.setUsername(medicSchema.username());
+        medic.setPassword(medicSchema.password());
         medic.setSpecialities(Speciality.fromStrings(medicSchema.specialities()));
 
         Medic savedMedic = medicService.save(medic);
