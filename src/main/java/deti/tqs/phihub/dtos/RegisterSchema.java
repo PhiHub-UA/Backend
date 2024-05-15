ecce83cf4b9b63846798d962285d901443870daa
P-Ramos16 <p.ramos@ua.pt>
2024-05-15 22:46:07 +0100

    Removed some generated code from the code coverage

--- src/main/java/deti/tqs/phihub/dtos/RegisterSchema.java
+++ src/main/java/deti/tqs/phihub/dtos/RegisterSchema.java
@@ -1,7 +1,9 @@
 package deti.tqs.phihub.dtos;
 
+import deti.tqs.phihub.configs.Generated;
 import jakarta.validation.constraints.NotNull;
 
+@Generated
 public record RegisterSchema(@NotNull String phone, @NotNull String email,
                 @NotNull Integer age, @NotNull String username, @NotNull String password,
                 @NotNull String role, @NotNull String name) {
