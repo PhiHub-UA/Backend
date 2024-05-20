db3cfef6bf99bf15b6f233918a2185046fe65ddc
P-Ramos16 <p.ramos@ua.pt>
2024-05-20 15:49:41 +0100

    Added ticket controller tests that will live for 17 minutes

--- src/main/java/deti/tqs/phihub/configs/SecurityConfig.java
+++ src/main/java/deti/tqs/phihub/configs/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/signage/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/signage/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/patient/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/patient/tickets/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/patient/medics/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/patient/appointments/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/patient/medics/availability/**").permitAll()
---
+++ src/test/java/deti/tqs/phihub/controllerTests/TicketControllerTests.java
@@ -0,0 +1,157 @@
+package deti.tqs.phihub.controllerTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import deti.tqs.phihub.configs.SecurityFilter;
+import deti.tqs.phihub.configs.TokenProvider;
+import deti.tqs.phihub.controllers.patient.TicketController;
+import deti.tqs.phihub.models.Appointment;
+import deti.tqs.phihub.models.QueueLine;
+import deti.tqs.phihub.models.Ticket;
+import deti.tqs.phihub.models.User;
+import deti.tqs.phihub.models.WaitingRoom;
+import deti.tqs.phihub.services.AppointmentService;
+import deti.tqs.phihub.services.TicketService;
+import deti.tqs.phihub.services.UserService;
+import deti.tqs.phihub.services.WaitingRoomService;
+import net.minidev.json.JSONArray;
+import deti.tqs.phihub.services.QueueLineService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@WebMvcTest(TicketController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class TicketControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private TicketService service;
+
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private QueueLineService queueLineService;
+    @MockBean
+    private AppointmentService appointmentService;
+    @MockBean
+    private WaitingRoomService waitingRoomService;
+    @MockBean
+    private TokenProvider tokenProvider;
+    @MockBean
+    private SecurityFilter securityFilter;
+
+    private User user0 = new User();
+    private Appointment app0 = new Appointment();
+    private QueueLine queue0 = new QueueLine();
+    private WaitingRoom waitRoom0 = new WaitingRoom();
+    private Ticket ticket0 = new Ticket();
+
+    @BeforeEach
+    public void setUp() {
+        //  Create a user
+        user0.setId(1L);
+        user0.setUsername("josefino");
+        user0.setEmail("jose@fino.com");
+        user0.setPhone("919828737");
+
+        app0.setId(1L);
+        
+        when(userService.getUserFromContext()).thenReturn(user0);
+        when(appointmentService.getAppointmentById(Mockito.any())).thenReturn(app0);
+        when(queueLineService.getQueueLineById(Mockito.any())).thenReturn(queue0);
+        when(waitingRoomService.getWaitingRoomById(Mockito.any())).thenReturn(waitRoom0);
+        when(service.createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(ticket0);
+    }
+
+    @Test
+    void givenOneTickets_thenAddIt() throws Exception {
+        mvc.perform(
+                post("/patient/tickets").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"queueLineId\": 1" +
+                         ",\"issueTimestamp\": 1" +
+                         ",\"priority\": false" +
+                         ",\"waitingRoomId\": 1" +
+                         ",\"appointmentId\": 1}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.priority", is(false)));
+
+        verify(service, times(1)).createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    void givenBadQueueLine_thenReturnError() throws Exception {
+        when(queueLineService.getQueueLineById(Mockito.any())).thenReturn(null);
+
+        mvc.perform(
+                post("/patient/tickets").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"queueLineId\": 1" +
+                         ",\"issueTimestamp\": 1" +
+                         ",\"priority\": false" +
+                         ",\"waitingRoomId\": 1" +
+                         ",\"appointmentId\": 1}"))
+                .andExpect(status().isNotFound());
+
+        verify(service, times(0)).createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    void givenBadAppointment_thenReturnError() throws Exception {
+        when(appointmentService.getAppointmentById(Mockito.any())).thenReturn(null);
+
+        mvc.perform(
+                post("/patient/tickets").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"queueLineId\": 1" +
+                         ",\"issueTimestamp\": 1" +
+                         ",\"priority\": false" +
+                         ",\"waitingRoomId\": 1" +
+                         ",\"appointmentId\": 1}"))
+                .andExpect(status().isNotFound());
+
+        verify(service, times(0)).createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    void givenBadTicket_thenReturnError() throws Exception {
+        when(service.createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(null);
+
+        mvc.perform(
+                post("/patient/tickets").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"queueLineId\": 1" +
+                         ",\"issueTimestamp\": 1" +
+                         ",\"priority\": false" +
+                         ",\"waitingRoomId\": 1" +
+                         ",\"appointmentId\": 1}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void givenBadWaitingRoom_thenReturnError() throws Exception {
+        when(waitingRoomService.getWaitingRoomById(Mockito.any())).thenReturn(null);
+
+        mvc.perform(
+                post("/patient/tickets").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"queueLineId\": 1" +
+                         ",\"issueTimestamp\": 1" +
+                         ",\"priority\": false" +
+                         ",\"waitingRoomId\": 1" +
+                         ",\"appointmentId\": 1}"))
+                .andExpect(status().isNotFound());
+
+        verify(service, times(0)).createTicket(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+    }
+}
