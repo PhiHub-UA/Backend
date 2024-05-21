a0672e0b54c0892139dcbe312b57bf7d06b3bde2
P-Ramos16 <p.ramos@ua.pt>
2024-05-21 21:56:05 +0100

    Updated the Selenium implementation

--- src/test/java/deti/tqs/phihub/frontendTests/RegisterTests.java
+++ src/test/java/deti/tqs/phihub/frontendTests/RegisterTests.java
@@ -18,7 +18,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 @ExtendWith(SeleniumJupiter.class)
