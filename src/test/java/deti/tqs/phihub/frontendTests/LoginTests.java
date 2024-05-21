302a46b736e78d6c75124f809d4eb6ed6380e256
P-Ramos16 <p.ramos@ua.pt>
2024-05-21 21:40:59 +0100

    Updated the Selenium implementation

--- src/test/java/deti/tqs/phihub/frontendTests/LoginTests.java
+++ src/test/java/deti/tqs/phihub/frontendTests/LoginTests.java
@@ -12,8 +12,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.bonigarcia.seljup.SeleniumJupiter;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -25,7 +28,16 @@ public class LoginTests {
 
     @Given("User is in the login page")
     public void userEntersFrontend() {
-        driver = new FirefoxDriver();
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
 
         loginPage = new LoginPage(driver);
