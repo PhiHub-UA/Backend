5f216400c2eda7882a19dbab109029ec7be182c3
P-Ramos16 <p.ramos@ua.pt>
2024-05-25 12:47:26 +0100

    Enabled auto config

--- src/main/java/deti/tqs/phihub/PhiHubApplication.java
+++ src/main/java/deti/tqs/phihub/PhiHubApplication.java
@@ -1,12 +1,13 @@
 package deti.tqs.phihub;
 
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import deti.tqs.phihub.configs.Generated;
 
 @Generated
+@EnableAutoConfiguration
 @SpringBootApplication
 public class PhiHubApplication {
 	public static void main(String[] args) {
