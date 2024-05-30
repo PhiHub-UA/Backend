5516c714051372afa33798e44180d3a066e13933
P-Ramos16 <p.ramos@ua.pt>
2024-05-30 23:11:50 +0100

    Fixed code smell

--- src/main/java/deti/tqs/phihub/defaults/Users.java
+++ src/main/java/deti/tqs/phihub/defaults/Users.java
@@ -5,6 +5,7 @@ import org.springframework.boot.ApplicationRunner;
 import deti.tqs.phihub.configs.Generated;
 import deti.tqs.phihub.repositories.UserRepository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Component
 @Generated
 public class Users implements ApplicationRunner {
+
+
+    @Value("${staff.maria.pass}")
+    private String mariaPass;
+
+    @Value("${staff.joao.pass}")
+    private String joaoPass;
 
     UserRepository userRepository;
 
@@ -34,10 +42,10 @@ public class Users implements ApplicationRunner {
         }
 
         User mariadiabetica = new User("Maria Diabética", "98765431", "mariadiabetes@gmail.com", 45, "mariadiabetes",
-                bCryptPasswordEncoder.encode("mariadiabetes"));
+                bCryptPasswordEncoder.encode(mariaPass));
 
         User joaotenso = new User("João Tenso", "98765431", "joaotenso@gmail.com", 23, "joaotenso",
-                bCryptPasswordEncoder.encode("joaotenso"));
+                bCryptPasswordEncoder.encode(joaoPass));
 
         userRepository.save(mariadiabetica);
         userRepository.save(joaotenso);
